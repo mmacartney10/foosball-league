039f86f7a715d4a7808904ecbb9785c7c4c34eb3
Matthew Macartney <matthewmacartney@CODE374.home>
2017-02-12 20:26:47 +0000

    Feat: added precss to project

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,9 @@
+/* eslint-disable */
+
 module.exports = {
   plugins: {
     'postcss-import': {},
+    'precss': {},
     'postcss-cssnext': {
       browsers: ['last 2 versions', '> 5%'],
     },
