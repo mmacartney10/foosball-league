b203547ff90d95ebf6a34e9d214f9ab57776f1f9
Matthew Macartney <matthewmacartney@CODE374.home>
2017-02-12 23:25:27 +0000

    Feat: addeed base styles, fixed issue with postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,24 @@
 /* eslint-disable */
 
 module.exports = {
+  syntax: 'postcss-scss',
   plugins: {
     'postcss-import': {},
+    'postcss-mixins': {},
     'precss': {},
     'stylelint': {},
-    'postcss-cssnext': {
+    'postcss-pxtorem': {
+      rootValue: 16,
+      unitPrecision: 5,
+      propList: ['*'],
+      selectorBlackList: [],
+      replace: true,
+      mediaQuery: false,
+      minPixelValue: 0
+    },
+    'autoprefixer': {
       browsers: ['last 2 versions', '> 5%'],
     },
+    'cssnano': true,
   },
 };
